f6ccddb640ce355bbb765daf9b56aefecace5dea
Ritik Dashore <ritikgithub04@gmail.com>
2026-05-02 18:24:50 +0530

    remove chang545

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ passport.deserializeUser(function(obj, cb) {
 });
     
 app.get('/', (req, res) => {
-  res.send(`Hello, World 1000 !
+  res.send(`Hello, World 1000 5!
     <a href="/auth/google">Login with Google</a>`);
 });
 
